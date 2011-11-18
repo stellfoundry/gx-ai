616df6a5691a3a98fb7ab6784210bd13402d2eea
nmandell <nmandell@28a56f14-469b-1442-acfb-e68ebda8abd8>
2011-11-18 07:15:23 +0000

    git-svn-id: svn://gk.umd.edu/noah/trunk@1524 28a56f14-469b-1442-acfb-e68ebda8abd8

--- GPU_NLPS/nlps_kernel.cu
+++ GPU_NLPS/nlps_kernel.cu
@@ -1,38 +1,35 @@
+//SWITCHED X AND Y -> a(ky,kx,z)
+
 #include <stdio.h>
 
 __global__ void deriv(cufftComplex* f, cufftComplex* fdx, cufftComplex* fdy, 
                       cufftComplex* g, cufftComplex* gdx, cufftComplex* gdy,  
                       float* kx, float* ky, int Nx, int Ny, int Nz) 
 {
-  int idx = __umul24(blockIdx.x,blockDim.x)+threadIdx.x;
   int idy = __umul24(blockIdx.y,blockDim.y)+threadIdx.y;
-  
-  
+  int idx = __umul24(blockIdx.x,blockDim.x)+threadIdx.x;
   
   for(int k = 0; k<Nz; k++) {
    if(idx<(Nx/2+1) && idy<Ny) {
     int index = idx + (Nx/2+1)*idy + Ny*(Nx/2+1)*k;
     
-    fdx[index].x = 0;
-    fdx[index].y = 0;
-    fdy[index].x = 0;
-    fdy[index].y = 0; 
+    
     
     //df/dx
-    fdx[index].x = -kx[index]*f[index].y;
-    fdx[index].y =  kx[index]*f[index].x;
+    fdx[index].x = -kx[idx]*f[index].y;
+    fdx[index].y =  kx[idx]*f[index].x;
     
     //df/dy
-    fdy[index].x = -ky[index]*f[index].y;
-    fdy[index].y =  ky[index]*f[index].x;
+    fdy[index].x = -ky[idy]*f[index].y;
+    fdy[index].y =  ky[idy]*f[index].x;
     
     //dg/dx
-    gdx[index].x = -kx[index]*g[index].y;
-    gdx[index].y =  kx[index]*g[index].x;
+    gdx[index].x = -kx[idx]*g[index].y;
+    gdx[index].y =  kx[idx]*g[index].x;
     
-    //dg/dy
-    gdy[index].x = -ky[index]*g[index].y;
-    gdy[index].y =  ky[index]*g[index].x;
+    //dg/dx
+    gdy[index].x = -ky[idy]*g[index].y;
+    gdy[index].y =  ky[idy]*g[index].x;
     
     
     /* //da/dx
@@ -50,12 +47,12 @@ __global__ void deriv(cufftComplex* f, cufftComplex* fdx, cufftComplex* fdy,
 __global__ void bracket(cufftReal* mult, cufftReal* fdx, cufftReal* fdy, 
                       cufftReal* gdx, cufftReal* gdy, float scaler, int Nx, int Ny, int Nz)
 {
-  int idx = blockIdx.x*blockDim.x+threadIdx.x;
   int idy = blockIdx.y*blockDim.y+threadIdx.y;
+  int idx = blockIdx.x*blockDim.x+threadIdx.x;
   
   for(int k=0; k<Nz; k++) {
    if(idx<Nx && idy<Ny) {
-    int index = idx + (Nx)*idy + Ny*(Nx)*k;
+    int index = idx + (Nx)*idy + Nx*(Ny)*k;
     
     mult[index] = scaler*( fdx[index]*gdy[index] - fdy[index]*gdx[index] ); 
     
@@ -64,7 +61,24 @@ __global__ void bracket(cufftReal* mult, cufftReal* fdx, cufftReal* fdy,
  }
 }  
      					      
-
+__global__ void kInit(float* kx, float* ky, int Nx, int Ny, int Nz) 
+{
+  int idy = blockIdx.y*blockDim.y+threadIdx.y;
+  int idx = blockIdx.x*blockDim.x+threadIdx.x;
+  
+  
+    if(idx<Nx/2+1 && idy<Ny) {
+      
+      if(idy<Ny/2+1) {
+        kx[idx] = idx;
+	ky[idy] = idy;
+      } else {
+        kx[idx] = idx;
+	ky[idy] = idy - Ny;
+      }
+    }
+      
+}       
      
                            
     
