9431e22e6f62b93df5a9bd488a3e109d0010ed43
nmandell <nmandell@28a56f14-469b-1442-acfb-e68ebda8abd8>
2012-01-21 17:46:51 +0000

    git-svn-id: svn://gk.umd.edu/noah/trunk@1551 28a56f14-469b-1442-acfb-e68ebda8abd8

---
+++ GRFX/reduc_kernel.cu
@@ -0,0 +1,116 @@
+__global__ void sum(cufftComplex* result, cufftComplex* a)
+{
+  //shared mem size = 8*8*8*sizeof(cufftComplex)
+  extern __shared__ cufftComplex result_s[];
+  //tid up to blockDim.x*blockDim.y*blockDim.z = 8*8*8
+  int tid = threadIdx.x + blockDim.x*threadIdx.y + blockDim.x*blockDim.y*threadIdx.z;
+    
+  if(tid<8*8*8) {
+    result_s[tid].x = 0;
+    result_s[tid].y = 0;
+    
+    result_s[tid] = a[blockIdx.x*blockDim.x*blockDim.y*blockDim.z+tid];
+    __syncthreads();
+    
+    for(int s=(blockDim.x*blockDim.y*blockDim.z)/2; s>0; s>>=1) {
+      if(tid<s) {
+        result_s[tid].x += result_s[tid+s].x;	
+	result_s[tid].y += result_s[tid+s].y;
+      }
+      __syncthreads();
+    }
+    
+    if(tid==0) {
+      result[blockIdx.x].x = result_s[0].x;
+      result[blockIdx.x].y = result_s[0].y;
+    }   
+  }
+}
+
+__global__ void maximum(cufftComplex* result, cufftComplex* a)
+{
+  //shared mem size = 8*8*8*sizeof(cufftComplex)
+  extern __shared__ cufftComplex result_s[];
+  //tid up to blockDim.x*blockDim.y*blockDim.z = 8*8*8
+  int tid = threadIdx.x + blockDim.x*threadIdx.y + blockDim.x*blockDim.y*threadIdx.z;
+    
+  if(tid<8*8*8) {
+    result_s[tid].x = 0;
+    result_s[tid].y = 0;
+    
+    result_s[tid] = a[blockIdx.x*blockDim.x*blockDim.y*blockDim.z+tid];
+    __syncthreads();
+    
+    for(int s=(blockDim.x*blockDim.y*blockDim.z)/2; s>0; s>>=1) {
+      if(tid<s) {
+        if(result_s[tid+s].x*result_s[tid+s].x+result_s[tid+s].y*result_s[tid+s].y >
+	        result_s[tid].x*result_s[tid].x+result_s[tid].y*result_s[tid].y) {
+				
+	  result_s[tid].x = result_s[tid+s].x;
+	  result_s[tid].y = result_s[tid+s].y;	
+	
+	}  
+      }
+      __syncthreads();
+    }
+    
+    if(tid==0) {
+      result[blockIdx.x].x = result_s[0].x; 
+      result[blockIdx.x].y = result_s[0].y;  
+    }   
+  }
+}
+
+
+__global__ void zeroPadded(cufftComplex* a) 
+{
+  unsigned int idx = __umul24(blockIdx.x,blockDim.x)+threadIdx.x;
+  unsigned int idy = __umul24(blockIdx.y,blockDim.y)+threadIdx.y;
+  unsigned int idz = __umul24(blockIdx.z,blockDim.z)+threadIdx.z;
+  
+  if(Nz<=zThreads) {
+    if(idx<Nx && idy<Ny && idz<Nz) {
+      int index = idy + Ny*idx + Nx*Ny*idz;
+      
+      a[index].x = 0;
+      a[index].y = 0;
+    }   
+  }
+  else {
+    for(int i=0; i<Nz/zThreads; i++) {
+      
+      if(idx<Nx && idy<Ny && idz<Nz) {
+        int index = idy + Ny*idx + Nx*Ny*idz + Nx*Ny*zThreads*i;
+	
+        a[index].x = 0;
+	a[index].y = 0;
+      }
+    }
+  }        
+}  
+
+__global__ void cleanPadded(cufftComplex* a) 
+{
+  unsigned int idx = __umul24(blockIdx.x,blockDim.x)+threadIdx.x;
+  unsigned int idy = __umul24(blockIdx.y,blockDim.y)+threadIdx.y;
+  unsigned int idz = __umul24(blockIdx.z,blockDim.z)+threadIdx.z;
+  
+  if(Nz<=zThreads) {
+    int index= idy + Ny*idx + Nx*Ny*idz;
+    if(index > Nx*(Ny/2+1)*Nz-1 && idx<Nx && idy<Ny && idz<Nz) {
+    
+      a[index].x = 0;
+      a[index].y = 0;
+    }   
+  }
+  else {
+    for(int i=0; i<Nz/zThreads; i++) {
+      int index = idy + Ny*idx + Nx*Ny*idz + Nx*Ny*zThreads*i;
+      if(index > Nx*(Ny/2+1)*Nz-1 && idx<Nx && idy<Ny && idz<Nz) {
+    
+        a[index].x = 0;
+	a[index].y = 0;
+      }
+    }
+  }        
+}  
---
+++ GRFX/sumReduc.cu
@@ -0,0 +1,52 @@
+void sumReduc(cufftComplex* result, cufftComplex* f, cufftComplex* padded) 
+{
+    int dev;
+    struct cudaDeviceProp prop;
+    cudaGetDevice(&dev);
+    cudaGetDeviceProperties(&prop,dev);
+    int zThreads = prop.maxThreadsDim[2];
+    int totalThreads = prop.maxThreadsPerBlock;   
+    
+    int xy = totalThreads/Nz;
+    int blockxy = sqrt(xy);
+    //dimBlock = threadsPerBlock, dimGrid = numBlocks
+    dim3 dimBlock2(blockxy,blockxy,Nz);
+    if(Nz>zThreads) {
+      dimBlock2.x = sqrt(totalThreads/zThreads);
+      dimBlock2.y = sqrt(totalThreads/zThreads);
+      dimBlock2.z = zThreads;
+    }  
+    
+    dim3 dimGrid2(Nx/dimBlock2.x+1,Ny/dimBlock2.y+1,1);
+    
+    cudaMemcpy(padded,f,sizeof(cufftComplex)*Nx*(Ny/2+1)*Nz,cudaMemcpyDeviceToDevice);
+    
+    cleanPadded<<<dimGrid2,dimBlock2>>>(padded);
+    
+    dim3 dimBlock(8,8,8);
+    int gridx = (Nx*Ny*Nz)/512;
+    
+    if (Nx*Ny*Nz <= 512) {
+      dimBlock.x = Nx;
+      dimBlock.y = Ny;
+      dimBlock.z = Nz;
+      gridx = 1;
+    }  
+    
+    dim3 dimGrid(gridx,1,1);
+    
+    sum<<<dimGrid,dimBlock,sizeof(cufftComplex)*8*8*8>>>(padded, padded);
+    
+    while(dimGrid.x > 512) {
+      dimGrid.x = dimGrid.x / 512;
+      sum<<<dimGrid,dimBlock,sizeof(cufftComplex)*8*8*8>>>(padded, padded);
+    }  
+    
+    dimBlock.x = dimGrid.x;
+    dimGrid.x = 1;
+    dimBlock.y = dimBlock.z = 1;
+    sum<<<dimGrid,dimBlock,sizeof(cufftComplex)*8*8*8>>>(padded,padded);  
+    
+    cudaMemcpy(result, padded, sizeof(cufftComplex), cudaMemcpyDeviceToHost);
+
+}    
