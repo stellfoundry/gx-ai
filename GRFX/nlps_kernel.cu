f03db1b8e41cd87590506d84bf8863e0af03a6b8
bdorland <bdorland@28a56f14-469b-1442-acfb-e68ebda8abd8>
2012-03-03 22:43:28 +0000

    Only changed comments
    git-svn-id: svn://gk.umd.edu/noah/trunk@1562 28a56f14-469b-1442-acfb-e68ebda8abd8

--- GRFX/nlps_kernel.cu
+++ GRFX/nlps_kernel.cu
@@ -24,11 +24,11 @@ __global__ void deriv(cufftComplex* f, cufftComplex* fdx, cufftComplex* fdy, flo
     if(idy<(Ny/2+1) && idx<Nx && idz<zThreads) {
     unsigned int index = idy + (Ny/2+1)*idx + Nx*(Ny/2+1)*idz + Nx*(Ny/2+1)*zThreads*i;
     
-    //df/dx
+    //df/dy
     fdy[index].x = -ky[idy]*f[index].y;			
     fdy[index].y =  ky[idy]*f[index].x;			
     
-    //df/dy
+    //df/dx
     fdx[index].x = -kx[idx]*f[index].y;			
     fdx[index].y =  kx[idx]*f[index].x;			
     }
