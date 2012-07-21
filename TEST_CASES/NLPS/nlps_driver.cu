ba4ba0030348ceb6632140eef70870e6f381d5ed
nmandell <nmandell@28a56f14-469b-1442-acfb-e68ebda8abd8>
2012-07-21 17:41:42 +0000

    git-svn-id: svn://gk.umd.edu/noah/trunk@1716 28a56f14-469b-1442-acfb-e68ebda8abd8

---
+++ TEST_CASES/NLPS/nlps_driver.cu
@@ -0,0 +1,192 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <cufft.h>
+
+__constant__ int Nx,Ny,Nz,zThreads, X0, Y0, Z0;
+dim3 dimGrid;
+dim3 dimBlock;
+int totalThreads;
+bool MASK;
+bool debug=true;
+
+//make sure GRFX, GPU_NLPS, and TEST_CASES are in same directory
+#include "getfcn.cu"
+#include "nlps_kernel.cu"
+#include "timestep_kernel.cu"
+#include "zderiv_kernel.cu"
+#include "nlps.cu"
+#include "nlpstest.cu"
+
+
+int main(int argc, char* argv[])
+{
+    int fkx, fky, gkx, gky, fsin, fcos, gsin, gcos;
+    cufftReal *nlps;
+    cufftReal *nlpscheck, *fdxcheck, *fdycheck, *gdxcheck, *gdycheck;
+    float *x, *y, *z;
+    
+    
+    int ct, dev;
+    struct cudaDeviceProp prop;
+    cudaGetDeviceCount(&ct);
+    printf("Device Count: %d\n",ct);
+    cudaGetDevice(&dev);
+    printf("Device ID: %d\n",dev);
+    cudaGetDeviceProperties(&prop,dev);
+    printf("Device Name: %s\n", prop.name);
+    printf("Global Memory (bytes): %lu\n", (unsigned long)prop.totalGlobalMem);
+    printf("Shared Memory per Block (bytes): %lu\n", (unsigned long)prop.sharedMemPerBlock);
+    printf("Registers per Block: %d\n", prop.regsPerBlock);
+    printf("Warp Size (threads): %d\n", prop.warpSize); 
+    printf("Max Threads per Block: %d\n", prop.maxThreadsPerBlock);
+    printf("Max Size of Block Dimension (threads): %d * %d * %d\n", prop.maxThreadsDim[0], 
+                        prop.maxThreadsDim[1], prop.maxThreadsDim[2]);
+    printf("Max Size of Grid Dimension (blocks): %d * %d * %d\n", prop.maxGridSize[0], prop.maxGridSize[1], prop.maxGridSize[2]);
+    
+    
+    MASK = false;
+    
+    
+    if ( argc != 3 ) // argc should be 2 for correct execution 
+    {
+        //We print argv[0] assuming it is the program name 
+        printf( "usage: %s inputfile outputfile", argv[0] );
+    }
+    else 
+    {
+        // We assume argv[1] is a filename to open
+        FILE *ifile = fopen( argv[1], "r" );
+
+        // fopen returns 0, the NULL pointer, on failure 
+        if ( ifile == 0 )
+        {
+            printf( "Could not open file\n" );
+        }
+        else 
+        {
+            fscanf(ifile, "%d %d %d %d %d %d %d %d %d %d %d %d %d %d", &fkx, &fky, &fsin, &fcos, &gkx, &gky, &gsin, &gcos, &Nx, &Ny, &Nz, &X0,&Y0,&Z0);
+            fclose( ifile );
+        } 
+	nlpscheck = (cufftReal*) malloc(sizeof(cufftReal)*Nx*Ny*Nz);        
+        nlps = (cufftReal*) malloc(sizeof(cufftReal)*Nx*Ny*Nz);
+	y = (float*) malloc(sizeof(float)*Ny);					//
+        x = (float*) malloc(sizeof(float)*Nx);
+	z = (float*) malloc(sizeof(float)*Nz);					//
+	fdxcheck = (cufftReal*) malloc(sizeof(cufftReal)*Nx*Ny*Nz);
+	fdycheck = (cufftReal*) malloc(sizeof(cufftReal)*Nx*Ny*Nz);
+	gdxcheck = (cufftReal*) malloc(sizeof(cufftReal)*Nx*Ny*Nz);
+	gdycheck = (cufftReal*) malloc(sizeof(cufftReal)*Nx*Ny*Nz);
+	
+	
+	
+	cudaMemcpyToSymbol("Nx", &Nx, sizeof(int),0,cudaMemcpyHostToDevice);
+	cudaMemcpyToSymbol("Ny", &Ny, sizeof(int),0,cudaMemcpyHostToDevice);
+	cudaMemcpyToSymbol("Nz", &Nz, sizeof(int),0,cudaMemcpyHostToDevice);
+	cudaMemcpyToSymbol("zThreads", &prop.maxThreadsDim[2], sizeof(int),0,cudaMemcpyHostToDevice);
+	cudaMemcpyToSymbol("X0", &X0, sizeof(int),0,cudaMemcpyHostToDevice);
+	cudaMemcpyToSymbol("Y0", &Y0, sizeof(int),0,cudaMemcpyHostToDevice);
+	cudaMemcpyToSymbol("Z0", &Z0, sizeof(int),0,cudaMemcpyHostToDevice);
+	
+	
+	*&zThreads = prop.maxThreadsDim[2];
+        totalThreads = prop.maxThreadsPerBlock;
+        int xy = totalThreads/Nz;
+        int blockxy = sqrt(xy);
+        //dimBlock = threadsPerBlock, dimGrid = numBlocks
+        dimBlock.x = blockxy;
+        dimBlock.y = blockxy;
+        dimBlock.z = Nz;
+        if(Nz>zThreads) {
+          dimBlock.x = sqrt(totalThreads/zThreads);
+          dimBlock.y = sqrt(totalThreads/zThreads);
+          dimBlock.z = zThreads;
+        }  
+    
+        dimGrid.x = Nx/dimBlock.x+1;
+        dimGrid.y = Ny/dimBlock.y+1;
+        dimGrid.z = 1;    
+	
+	
+	
+	nlps = NLPStest(fkx, fky, fsin, fcos, gkx, gky, gsin, gcos);
+	if(debug) {printf("Executed NLPStest\n");}
+        
+	for(int k=0; k<Nz; k++) {
+	 for(int j=0; j<Nx; j++) {
+	  for(int i=0; i<Ny; i++) {
+	    y[i] = 2*M_PI*(float)(i-Ny/2)/Ny;					//
+            x[j] = 2*M_PI*(float)(j-Nx/2)/Nx;					//
+            z[k] = 2*M_PI*(float)(k-Nz/2)/Nz;
+	    int index = i + Ny*j + Nx*Ny*k;
+	    
+	    //(df/dx)(dg/dy)-(df/dy)(dg/dx) 
+	    fdxcheck[index] = -fkx*fcos*sin(fky*y[i] + fkx*x[j]) + fkx*fsin*cos(fky*y[i] + fkx*x[j]);	// only x and y changed,
+	    fdycheck[index] = -fky*fcos*sin(fky*y[i] + fkx*x[j]) + fky*fsin*cos(fky*y[i] + fkx*x[j]);	// fkx,fky,gkx,gky,i,j same
+	    gdxcheck[index] = -gkx*gcos*sin(gky*y[i] + gkx*x[j]) + gkx*gsin*cos(gky*y[i] + gkx*x[j]);	//
+	    gdycheck[index] = -gky*gcos*sin(gky*y[i] + gkx*x[j]) + gky*gsin*cos(gky*y[i] + gkx*x[j]);	//
+	    
+	    
+	    
+	    nlpscheck[index] = fdxcheck[index]*gdycheck[index] - fdycheck[index]*gdxcheck[index];
+	    
+	  }
+	 }     
+	}
+	FILE *ofile = fopen( argv[2], "w+");
+	
+	fprintf(ofile,"f(y,x)= %d*cos(%dy + %dx) + %d*sin(%dy + %dx)\ng(y,x)= %d*cos(%dy + %dx) + %d*sin(%dy + %dx)\nNx=%d, Ny=%d, Nz=%d\n\nOutputs:\nNLPS BRACKET\n",
+	                      fcos,fky,fkx,fsin,fky,fkx,gcos,gky,gkx,gsin,gky,gkx,Nx,Ny,Nz);
+	
+	for(int k=0; k<Nz; k++) {
+	 for(int j=0; j<Nx; j++) {
+          for(int i=0; i<Ny; i++) {
+            int index = i + Ny*j + Nx*Ny*k;
+            fprintf(ofile,"N(%.2fPI,%.2fPI,%.2fPI)=%.3f: %d  ", y[i]/M_PI,                      //
+	              x[j]/M_PI, z[k]/M_PI, nlps[index], index);     			        	 //
+          }
+          fprintf(ofile,"\n");
+         }
+	 
+	} 
+	
+	fprintf(ofile,"\nExpected values:\n(df/dx)(dg/dy)-(df/dy)(dg/dx)\n");
+	                       
+	
+	for(int k=0; k<Nz; k++) {
+	 for(int j=0; j<Nx; j++) {
+          for(int i=0; i<Ny; i++) {
+            int index = i + Ny*j + Nx*Ny*k;
+            fprintf(ofile,"N(%.2fPI,%.2fPI,%.2fPI)=%.3f: %d  ", y[i]/M_PI, 			//
+	              x[j]/M_PI, z[k]/M_PI, nlpscheck[index], index);     			 	//
+          }	
+          fprintf(ofile,"\n");
+	 } 
+	 fprintf(ofile,"\n");
+        }
+	
+	
+	
+	bool equal = true;
+	for(int k=0; k<Nz; k++) { 
+	 for(int j=0; j<Nx; j++) {
+	  for(int i=0; i<Ny; i++) {
+	    int index = i + Ny*j + Nx*Ny*k;
+	    if(abs(nlpscheck[index] - nlps[index]) > .5) {equal = false; fprintf(ofile, "%d\n",index);}
+	    if(nlps[index] != nlps[index]) {equal = false;}      //check for nan
+	  }
+	  
+	 }
+	 
+	}     
+	if(equal == true) {fprintf(ofile, "\nNLPS CHECKS\n"); printf("NLPS CHECKS\n");}
+	else {fprintf(ofile, "\nNLPS DOES NOT CHECK\n"); printf("NLPS DOES NOT CHECK\n");}
+	
+	printf("fkx=%d  fky=%d  fsin=%d  fcos=%d  gkx=%d  gky=%d  gsin=%d  gcos=%d  Nx=%d  Ny=%d  Nz=%d\n", fkx, fky, fsin,fcos,gkx, gky,gsin,gcos,Nx, Ny, Nz);
+	
+	fclose(ofile);
+	        
+    }
+}
+  
