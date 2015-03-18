b2c7e4ebce6ba71bcfd6b04b96a97bc03b9e5e24
nmandell <nmandell@28a56f14-469b-1442-acfb-e68ebda8abd8>
2015-03-18 21:01:11 +0000

    debugging unity...
    git-svn-id: svn://gk.umd.edu/noah/branches/gryfx_march15@2533 28a56f14-469b-1442-acfb-e68ebda8abd8

--- tests/nonlinear_tests/secondary/secondary.cu
+++ tests/nonlinear_tests/secondary/secondary.cu
@@ -1,18 +1,18 @@
 #include <unity.h>
-#include "global_vars.h"
+#include <setjmp.h>
 #include "gryfx_lib.h"
 
 /*int main(int argc, char ** argv) {*/
 /*return 0;*/
 /*}*/
 
-int mpcom;
+int mpcom_glob;
 int argc_glob;
 char** argv_glob;
 
 void setUp(void)
 {
-  gryfx_main(argc_glob, argv_glob, mpcom);
+  gryfx_main(argc_glob, argv_glob, mpcom_glob);
 }
  
 void tearDown(void)
@@ -27,18 +27,20 @@ int main(int argc, char* argv[])
 {
 #ifdef GS2_zonal
         MPI_Init(&argc, &argv);
-        mpcom = MPI_Comm_c2f(MPI_COMM_WORLD);
+        mpcom_glob = MPI_Comm_c2f(MPI_COMM_WORLD);
 #endif
   
   argc_glob = argc;
   argv_glob = argv;
 
-  UnityBegin("tests/nonlinear_tests/secondary.c");
+  UnityBegin("tests/nonlinear_tests/secondary.cu");
 
   RUN_TEST(test_secondary_growth_rate);
+  UnityEnd();
 #ifdef GS2_zonal
         MPI_Finalize();
 #endif
+   
 	return 0;
 
 }
