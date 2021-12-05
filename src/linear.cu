stellfoundry/gx-ai

commit b205626ab5cd8d94baa8b914c7d49b99e8d6e104

replacing cudaMallocHost with malloc everywhere. we do not want/need to use the special features of cudaMallocHost (which allows page-locked host memory to be read directly by device) so it is better to not waste memory available to the system for paging. there is a small possibility that this commit breaks something if there was a place where memory that had been allocated with cudaMallocHost was being passed directly to a kernel. this was the case in some places in diagnostics, but this was changed in the previous commit.

src/linear.cu
@@ -54,9 +54,14 @@ Linear::Linear(Parameters* pars, Grids* grids, Geometry* geo) :
     cudaMalloc((void**) &s11, sizeof(float)*grids_->Nz);
     cudaMalloc((void**) &vol_fac, sizeof(float)*grids_->Nz);
     
-    volDenom = 0. ;  cudaMallocHost (&vol_fac, sizeof(float) * grids_->Nz);
-    for (int i=0; i < grids_->Nz; i++) volDenom   += geo_->jacobian_h[i]; 
-    for (int i=0; i < grids_->Nz; i++) vol_fac[i]  = geo_->jacobian_h[i] / volDenom;
+    volDenom = 0.;  
+    float *vol_fac_h;
+    vol_fac_h = (float*) malloc (sizeof(float) * nZ);
+    cudaMalloc (&vol_fac, sizeof(float) * nZ);
+    for (int i=0; i < nZ; i++) volDenom   += geo_->jacobian_h[i]; 
+    for (int i=0; i < nZ; i++) vol_fac_h[i]  = geo_->jacobian_h[i] / volDenom;
+    CP_TO_GPU(vol_fac, vol_fac_h, sizeof(float)*nZ);
+    free(vol_fac_h);
   }
   
   // allocate conservation terms for collision operator
@@ -151,6 +156,7 @@ Linear::~Linear()
   if (upar_bar)   cudaFree(upar_bar);
   if (uperp_bar)  cudaFree(uperp_bar);
   if (t_bar)      cudaFree(t_bar);
+  if (vol_fac)    cudaFree(vol_fac);
 }
 
 void Linear::rhs(MomentsG* G, Fields* f, MomentsG* GRhs) {

include/nonlinear.h
@@ -51,8 +51,8 @@ class Nonlinear {
   float * dJ0_Apar_dy ;
   float * dphi        ;
   float * g_res       ;
-  float * vmax_x      ;
-  float * vmax_y      ;
+  float vmax_x[1]     ;
+  float vmax_y[1]     ;
   cuComplex * J0phi   ;
   cuComplex * J0_Apar ;
 };

src/diagnostics.cu
@@ -82,7 +82,7 @@ Diagnostics::Diagnostics(Parameters* pars, Grids* grids, Geometry* geo) :
   // need if (pars_->write_flux || "diagnosing potential) {
   cudaMalloc (&P2s, sizeof(float) * nR * nS);
 
-  if (id -> rh -> write) cudaMallocHost(&val, sizeof(float)*2);
+  if (id -> rh -> write) val = (float*) malloc(sizeof(float)*2);
 
   if (!pars_->all_kinetic) {
 
@@ -107,9 +107,9 @@ Diagnostics::Diagnostics(Parameters* pars, Grids* grids, Geometry* geo) :
   
   /*
   if (pars_->diagnosing_pzt) {
-    cudaMallocHost (&primary,   sizeof(float));    primary[0] = 0.;  
-    cudaMallocHost (&secondary, sizeof(float));    secondary[0] = 0.;
-    cudaMallocHost (&tertiary,  sizeof(float));    tertiary[0] = 0.;
+    primary = (float*) malloc (sizeof(float));    primary[0] = 0.;  
+    secondary = (float*) malloc (sizeof(float));  secondary[0] = 0.;  
+    tertiary = (float*) malloc (sizeof(float));    tertiary[0] = 0.;  
     cudaMalloc     (&t_bar,     sizeof(cuComplex) * nR * nS);
   }
   */
@@ -160,10 +160,10 @@ Diagnostics::Diagnostics(Parameters* pars, Grids* grids, Geometry* geo) :
   
   if (pars_->ks) {
     cudaMalloc     (&gy_d, sizeof(float)*grids_->Ny);
-    cudaMallocHost (&gy_h, sizeof(float)*grids_->Ny);
+    gy_h = (float*) malloc (sizeof(float)*grids_->Ny);
 
     if (pars_->ResWrite) {
-      cudaMallocHost (&ry_h, sizeof(double)*pars_->ResQ*grids_->NxNyNz*grids_->Nmoms);
+      ry_h = (double*) malloc(sizeof(double)*pars_->ResQ*grids_->NxNyNz*grids_->Nmoms);
     }
   }
   
@@ -192,10 +192,10 @@ Diagnostics::~Diagnostics()
   if (vol_fac)    cudaFree  ( vol_fac   );
   if (flux_fac)   cudaFree  ( flux_fac  );
   if (kvol_fac)   cudaFree  ( kvol_fac  );
-  if (val)        cudaFreeHost  ( val       );
+  if (val)        free  ( val       );
   if (tmp_omg_h)  free  ( tmp_omg_h );
-  if (gy_h)       cudaFreeHost  ( gy_h      );
-  if (ry_h)       cudaFreeHost  ( ry_h      );
+  if (gy_h)       free  ( gy_h      );
+  if (ry_h)       free  ( ry_h      );
 }
 
 bool Diagnostics::loop(MomentsG* G, Fields* fields, double dt, int counter, double time) 
@@ -298,7 +298,7 @@ bool Diagnostics::loop(MomentsG* G, Fields* fields, double dt, int counter, doub
 
       if (pars_->ks) {
 	cuComplex * g_h;
-	cudaMallocHost (&g_h, sizeof(cuComplex)*grids_->Nyc);
+	g_h = (cuComplex*) malloc (sizeof(cuComplex)*grids_->Nyc);
 	CP_TO_CPU(g_h, G->G(), sizeof(cuComplex)*grids_->Nyc);
 	float Dtmp = 0.;
 	for (int i=0; i<grids_->Naky; i++) {
@@ -307,7 +307,7 @@ bool Diagnostics::loop(MomentsG* G, Fields* fields, double dt, int counter, doub
 	Dks += Dtmp;
 	printf("<D> = %f \t",Dks/((float) ndiag));
 	ndiag += 1;
-	cudaFreeHost  (g_h);
+	free(g_h);
       }
       
       id->write_Wm    (G2   );    id->write_Wl    (G2   );    id->write_Wlm   (G2   );    

src/fields.cu
@@ -12,22 +12,22 @@ Fields::Fields(Parameters* pars, Grids* grids) :
 
   //  cudaMemset(phi, 0., size_);
 
-  cudaMallocHost((void**) &phi_h, size_);
+  phi_h = (cuComplex*) malloc(size_);
 
   if (pars_->beta > 0.) {
     checkCuda(cudaMalloc((void**) &apar, size_));
 
     cudaMemset(apar, 0., size_); setval <<< nb, nt >>> (apar, zero, nn);
 
-    cudaMallocHost((void**) &apar_h, size_);
+    apar_h = (cuComplex*) malloc(size_);
   }
 }
 
 Fields::~Fields() {
   if (phi)     cudaFree(phi);
-  if (phi_h)   cudaFreeHost(phi_h);
+  if (phi_h)   free(phi_h);
   if (apar)    cudaFree(apar);
-  if (apar_h)  cudaFreeHost(apar_h);
+  if (apar_h)  free(apar_h);
 }
 
 void Fields::print_phi(void)

src/geometry.cu
@@ -47,19 +47,19 @@ Geometry::~Geometry() {
   if (grho);     cudaFree(grho);	
   if (jacobian); cudaFree(jacobian);	
 
-  if (z_h)         cudaFreeHost(z_h);
-  if (bmag_h)      cudaFreeHost(bmag_h);
-  if (bmagInv_h)   cudaFreeHost(bmagInv_h);
-  if (bgrad_h)     cudaFreeHost(bgrad_h);
-  if (gds2_h);     cudaFreeHost(gds2_h);	
-  if (gds21_h);    cudaFreeHost(gds21_h);	
-  if (gds22_h);    cudaFreeHost(gds22_h);	
-  if (gbdrift_h);  cudaFreeHost(gbdrift_h);	
-  if (gbdrift0_h); cudaFreeHost(gbdrift0_h);	
-  if (cvdrift_h);  cudaFreeHost(cvdrift_h);	
-  if (cvdrift0_h); cudaFreeHost(cvdrift0_h);	
-  if (grho_h);     cudaFreeHost(grho_h);	
-  if (jacobian_h); cudaFreeHost(jacobian_h);	
+  if (z_h)         free(z_h);
+  if (bmag_h)      free(bmag_h);
+  if (bmagInv_h)   free(bmagInv_h);
+  if (bgrad_h)     free(bgrad_h);
+  if (gds2_h);     free(gds2_h);	
+  if (gds21_h);    free(gds21_h);	
+  if (gds22_h);    free(gds22_h);	
+  if (gbdrift_h);  free(gbdrift_h);	
+  if (gbdrift0_h); free(gbdrift0_h);	
+  if (cvdrift_h);  free(cvdrift_h);	
+  if (cvdrift0_h); free(cvdrift0_h);	
+  if (grho_h);     free(grho_h);	
+  if (jacobian_h); free(jacobian_h);	
 
   if(operator_arrays_allocated_) {
     if (kperp2) cudaFree(kperp2);
@@ -75,21 +75,21 @@ S_alpha_geo::S_alpha_geo(Parameters *pars, Grids *grids)
   float theta;
   operator_arrays_allocated_=false;
   size_t size = sizeof(float)*Nz;
-  cudaMallocHost ((void**) &z_h, size);
-  cudaMallocHost ((void**) &bmag_h, size);
-  cudaMallocHost ((void**) &bmagInv_h, size);
-  cudaMallocHost ((void**) &bgrad_h, size);
-  cudaMallocHost ((void**) &gds2_h, size);
-  cudaMallocHost ((void**) &gds21_h, size);
-  cudaMallocHost ((void**) &gds22_h, size);
-  cudaMallocHost ((void**) &gbdrift_h, size);
-  cudaMallocHost ((void**) &gbdrift0_h, size);
-  cudaMallocHost ((void**) &cvdrift_h, size);
-  cudaMallocHost ((void**) &cvdrift0_h, size);
-  cudaMallocHost ((void**) &grho_h, size);
-  cudaMallocHost ((void**) &jacobian_h, size);
+  z_h = (float*) malloc (size);
+  bmag_h = (float*) malloc (size);
+  bmagInv_h = (float*) malloc (size);
+  bgrad_h = (float*) malloc (size);
+  gds2_h = (float*) malloc (size);
+  gds21_h = (float*) malloc (size);
+  gds22_h = (float*) malloc (size);
+  gbdrift_h = (float*) malloc (size);
+  gbdrift0_h = (float*) malloc (size);
+  cvdrift_h = (float*) malloc (size);
+  cvdrift0_h = (float*) malloc (size);
+  grho_h = (float*) malloc (size);
+  jacobian_h = (float*) malloc (size);
 
-  //  cudaMallocHost((void**) &kperp2_h, sizeof(float)*grids->NxNycNz);
+  // kperp2_h = (float*) malloc(sizeof(float)*grids->NxNycNz);
   
   cudaMalloc ((void**) &z, size);
   cudaMalloc ((void**) &bmag, size);
@@ -223,19 +223,28 @@ geo_nc::geo_nc(Parameters *pars, Grids *grids)
 
   // allocate space for variables on the CPU
   double * dtmp;
-  cudaMallocHost ((void**) &dtmp, sizeof(double)*N);
+  dtmp = (double*) malloc(sizeof(double)*N);
+  z_h = (float*) malloc (size);
+  bmag_h = (float*) malloc (size);
+  bmagInv_h = (float*) malloc (size);
+  gds2_h = (float*) malloc (size);
+  gds21_h = (float*) malloc (size);
+  gds22_h = (float*) malloc (size);
+  gbdrift_h = (float*) malloc (size);
+  gbdrift0_h = (float*) malloc (size);
+  cvdrift_h = (float*) malloc (size);
+  cvdrift0_h = (float*) malloc (size);
+  grho_h = (float*) malloc (size);
+  jacobian_h = (float*) malloc (size);
   
   // read the data with nc_get_var
   int id;
   if (retval = nc_inq_varid(ncgeo, "theta", &id))        ERR(retval);
   if (retval = nc_get_var  (ncgeo, id, dtmp))            ERR(retval);
-  cudaMallocHost ((void**) &z_h, size);
   for (int n=0; n<N; n++) z_h[n] = (float) dtmp[n];
   
   if (retval = nc_inq_varid(ncgeo, "bmag", &id))         ERR(retval);
   if (retval = nc_get_var  (ncgeo, id, dtmp))            ERR(retval);
-  cudaMallocHost ((void**) &bmag_h, size);
-  cudaMallocHost ((void**) &bmagInv_h, size);
   for (int n=0; n<N; n++) bmag_h[n] = (float) dtmp[n];
   for (int n=0; n<N; n++) bmagInv_h[n] = 1./bmag_h[n];
 
@@ -245,45 +254,37 @@ geo_nc::geo_nc(Parameters *pars, Grids *grids)
 
   if (retval = nc_inq_varid(ncgeo, "grho", &id))         ERR(retval);
   if (retval = nc_get_var  (ncgeo, id, dtmp))            ERR(retval);
-  cudaMallocHost ((void**) &grho_h, size);
   for (int n=0; n<N; n++) grho_h[n] = (float) dtmp[n];
   
   if (retval = nc_inq_varid(ncgeo, "gds2", &id))         ERR(retval);
   if (retval = nc_get_var  (ncgeo, id, dtmp))            ERR(retval);
-  cudaMallocHost ((void**) &gds2_h, size);
   for (int n=0; n<N; n++) gds2_h[n] = (float) dtmp[n];
   
   if (retval = nc_inq_varid(ncgeo, "gds21", &id))        ERR(retval);
   if (retval = nc_get_var  (ncgeo, id, dtmp))            ERR(retval);
-  cudaMallocHost ((void**) &gds21_h, size);
   for (int n=0; n<N; n++) gds21_h[n] = (float) dtmp[n];
   
   if (retval = nc_inq_varid(ncgeo, "gds22", &id))        ERR(retval);
   if (retval = nc_get_var  (ncgeo, id, dtmp))            ERR(retval);
-  cudaMallocHost ((void**) &gds22_h, size);
   for (int n=0; n<N; n++) gds22_h[n] = (float) dtmp[n];
   
   if (retval = nc_inq_varid(ncgeo, "gbdrift", &id))      ERR(retval);
   if (retval = nc_get_var  (ncgeo, id, dtmp))            ERR(retval);
-  cudaMallocHost ((void**) &gbdrift_h, size);
   for (int n=0; n<N; n++) gbdrift_h[n] = (float) dtmp[n];
   
   if (retval = nc_inq_varid(ncgeo, "gbdrift0", &id))     ERR(retval);
   if (retval = nc_get_var  (ncgeo, id, dtmp))            ERR(retval);
-  cudaMallocHost ((void**) &gbdrift0_h, size);
   for (int n=0; n<N; n++) gbdrift0_h[n] = (float) dtmp[n];
   
   if (retval = nc_inq_varid(ncgeo, "cvdrift", &id))      ERR(retval);
   if (retval = nc_get_var  (ncgeo, id, dtmp))            ERR(retval);
-  cudaMallocHost ((void**) &cvdrift_h, size);
   for (int n=0; n<N; n++) cvdrift_h[n] = (float) dtmp[n];
   
   if (retval = nc_inq_varid(ncgeo, "cvdrift0", &id))     ERR(retval);
   if (retval = nc_get_var  (ncgeo, id, dtmp))            ERR(retval);
-  cudaMallocHost ((void**) &cvdrift0_h, size);
   for (int n=0; n<N; n++) cvdrift0_h[n] = (float) dtmp[n];
   
-  cudaFreeHost(dtmp);
+  free(dtmp);
 
   double stmp; 
   
@@ -292,7 +293,6 @@ geo_nc::geo_nc(Parameters *pars, Grids *grids)
   pars->drhodpsi = (float) stmp;
   drhodpsi = pars->drhodpsi;
   
-  cudaMallocHost ((void**) &jacobian_h, size);
   for (int n=0; n<N; n++) jacobian_h[n] = 1./abs(drhodpsi*gradpar*bmag_h[n]);
       
   if (retval = nc_inq_varid(ncgeo, "kxfac", &id))        ERR(retval);
@@ -360,18 +360,18 @@ File_geo::File_geo(Parameters *pars, Grids *grids)
 
   operator_arrays_allocated_=false;
   size_t size = sizeof(float)*grids->Nz; 
-  cudaMallocHost ((void**) &z_h, size);
-  cudaMallocHost ((void**) &bmag_h, size);
-  cudaMallocHost ((void**) &bmagInv_h, size);
-  cudaMallocHost ((void**) &gds2_h, size);
-  cudaMallocHost ((void**) &gds21_h, size);
-  cudaMallocHost ((void**) &gds22_h, size);
-  cudaMallocHost ((void**) &gbdrift_h, size);
-  cudaMallocHost ((void**) &gbdrift0_h, size);
-  cudaMallocHost ((void**) &cvdrift_h, size);
-  cudaMallocHost ((void**) &cvdrift0_h, size);
-  cudaMallocHost ((void**) &grho_h, size);
-  cudaMallocHost ((void**) &jacobian_h, size);
+  z_h = (float*) malloc (size);
+  bmag_h = (float*) malloc (size);
+  bmagInv_h = (float*) malloc (size);
+  gds2_h = (float*) malloc (size);
+  gds21_h = (float*) malloc (size);
+  gds22_h = (float*) malloc (size);
+  gbdrift_h = (float*) malloc (size);
+  gbdrift0_h = (float*) malloc (size);
+  cvdrift_h = (float*) malloc (size);
+  cvdrift0_h = (float*) malloc (size);
+  grho_h = (float*) malloc (size);
+  jacobian_h = (float*) malloc (size);
 
   cudaMalloc ((void**) &z, size);
   cudaMalloc ((void**) &bmag, size);
@@ -544,7 +544,7 @@ void Geometry::initializeOperatorArrays(Grids* grids) {
   init_omegad GGEO (omegad, cv_d, gb_d, grids->kx, grids->ky, cvdrift, gbdrift, cvdrift0, gbdrift0, shat);
 
   /*
-  cudaMallocHost((void**) &kperp2_h, sizeof(float)*grids->NxNycNz);
+  kperp2_h = (float*) malloc(sizeof(float)*grids->NxNycNz);
   CP_TO_GPU (kperp2_h,    kperp2, sizeof(float)*grids->NxNycNz);
 
   for (int iz=0; iz < grids->Nz; iz++) {
@@ -565,7 +565,7 @@ void Geometry::calculate_bgrad(Grids* grids)
   operator_arrays_allocated_=false;
 
   size_t size = sizeof(float)*grids->Nz;
-  cudaMallocHost((void**) &bgrad_h, size);
+  bgrad_h = (float*) malloc (size);
 
   cudaMalloc ((void**) &bgrad, size);
   cudaMalloc ((void**) &bgrad_temp, size);

src/grad_parallel_linked.cu
@@ -57,23 +57,23 @@ GradParallelLinked::GradParallelLinked(Grids* grids, int jtwist)
   dG = (dim3*) malloc(sizeof(dim3)*nClasses);
   dB = (dim3*) malloc(sizeof(dim3)*nClasses);
 
-  cudaMallocHost ((void**) &zft_plan_forward,              sizeof(cufftHandle*)*nClasses);
-  cudaMallocHost ((void**) &zft_plan_inverse,              sizeof(cufftHandle*)*nClasses);
-  cudaMallocHost ((void**) &zft_plan_forward_singlemom,    sizeof(cufftHandle*)*nClasses);
-  //  cudaMallocHost ((void**) &zft_plan_inverse_singlemom,    sizeof(cufftHandle*)*nClasses);
+  zft_plan_forward = (cufftHandle*) malloc(sizeof(cufftHandle*)*nClasses);
+  zft_plan_inverse = (cufftHandle*) malloc(sizeof(cufftHandle*)*nClasses);
+  zft_plan_forward_singlemom = (cufftHandle*) malloc(sizeof(cufftHandle*)*nClasses);
+  // zft_plan_inverse_singlemom = (cufftHandle*) malloc(sizeof(cufftHandle*)*nClasses);
 
-  cudaMallocHost ((void**) &dz_plan_forward,               sizeof(cufftHandle*)*nClasses);
-  cudaMallocHost ((void**) &dz_plan_inverse,               sizeof(cufftHandle*)*nClasses);
-  cudaMallocHost ((void**) &dz_plan_forward_singlemom,     sizeof(cufftHandle*)*nClasses);
-  cudaMallocHost ((void**) &dz_plan_inverse_singlemom,     sizeof(cufftHandle*)*nClasses);
+  dz_plan_forward = (cufftHandle*) malloc(sizeof(cufftHandle*)*nClasses);
+  dz_plan_inverse = (cufftHandle*) malloc(sizeof(cufftHandle*)*nClasses);
+  dz_plan_forward_singlemom = (cufftHandle*) malloc(sizeof(cufftHandle*)*nClasses);
+  dz_plan_inverse_singlemom = (cufftHandle*) malloc(sizeof(cufftHandle*)*nClasses);
 
-  cudaMallocHost ((void**) &abs_dz_plan_forward_singlemom, sizeof(cufftHandle*)*nClasses);
+  abs_dz_plan_forward_singlemom = (cufftHandle*) malloc(sizeof(cufftHandle*)*nClasses);
 
   // these are arrays of pointers to device memory
-  cudaMallocHost ((void**) &ikxLinked, sizeof(int*)      *nClasses);
-  cudaMallocHost ((void**) &ikyLinked, sizeof(int*)      *nClasses);
-  cudaMallocHost ((void**) &G_linked,  sizeof(cuComplex*)*nClasses);
-  cudaMallocHost ((void**) &kzLinked,  sizeof(float*)    *nClasses);
+  ikxLinked = (int**) malloc(sizeof(int*)*nClasses);
+  ikyLinked = (int**) malloc(sizeof(int*)*nClasses);
+  G_linked = (cuComplex**) malloc(sizeof(cuComplex*)*nClasses);
+  kzLinked = (float**) malloc(sizeof(float*)*nClasses);
 
   //  printf("nClasses = %d\n", nClasses);
   for(int c=0; c<nClasses; c++) {
@@ -176,24 +176,24 @@ GradParallelLinked::~GradParallelLinked()
     if (kzLinked[c])          cudaFree(kzLinked[c]);
     if (G_linked[c])          cudaFree(G_linked[c]);
   }
-  if (zft_plan_forward)              cudaFreeHost(    zft_plan_forward);
-  if (zft_plan_inverse)              cudaFreeHost(    zft_plan_inverse);
-  if (zft_plan_forward_singlemom)    cudaFreeHost(    zft_plan_forward_singlemom);
-  //  if (zft_plan_inverse_singlemom)    cudaFreeHost(    zft_plan_inverse_singlemom);
+  if (zft_plan_forward)              free(    zft_plan_forward);
+  if (zft_plan_inverse)              free(    zft_plan_inverse);
+  if (zft_plan_forward_singlemom)    free(    zft_plan_forward_singlemom);
+  //  if (zft_plan_inverse_singlemom)    free(    zft_plan_inverse_singlemom);
 
-  if (dz_plan_forward)               cudaFreeHost(    dz_plan_forward);
-  if (dz_plan_inverse)               cudaFreeHost(    dz_plan_inverse);
-  if (dz_plan_forward_singlemom)     cudaFreeHost(    dz_plan_forward_singlemom);
-  if (dz_plan_inverse_singlemom)     cudaFreeHost(    dz_plan_inverse_singlemom);
-  if (abs_dz_plan_forward_singlemom) cudaFreeHost(abs_dz_plan_forward_singlemom);
+  if (dz_plan_forward)               free(    dz_plan_forward);
+  if (dz_plan_inverse)               free(    dz_plan_inverse);
+  if (dz_plan_forward_singlemom)     free(    dz_plan_forward_singlemom);
+  if (dz_plan_inverse_singlemom)     free(    dz_plan_inverse_singlemom);
+  if (abs_dz_plan_forward_singlemom) free(abs_dz_plan_forward_singlemom);
 
   if (ikxLinked_h)         free(ikxLinked_h);
   if (ikyLinked_h)         free(ikyLinked_h);
 
-  if (ikxLinked)           cudaFreeHost(ikxLinked);
-  if (ikyLinked)           cudaFreeHost(ikyLinked);
-  if (G_linked)            cudaFreeHost(G_linked);
-  if (kzLinked)            cudaFreeHost(kzLinked);
+  if (ikxLinked)           free(ikxLinked);
+  if (ikyLinked)           free(ikyLinked);
+  if (G_linked)            free(G_linked);
+  if (kzLinked)            free(kzLinked);
 }
 
 void GradParallelLinked::dealias(MomentsG* G)

src/grids.cu
@@ -34,21 +34,21 @@ Grids::Grids(Parameters* pars) :
   
   checkCuda(cudaDeviceSynchronize());
 
-  checkCuda(cudaMallocHost ( (void**) &kx_outh, sizeof(float) * Nakx )); 
+  kx_outh   = (float*) malloc(sizeof(float) * Nakx       );
   cudaMalloc     ( (void**) &kzm,       sizeof(int)   * Nz       );
   cudaMalloc     ( (void**) &kzp,       sizeof(float) * Nz       );
-  cudaMallocHost ( (void**) &kz_outh,   sizeof(float) * Nz       );
-  cudaMallocHost ( (void**) &kpar_outh, sizeof(float) * Nz       );
-  cudaMallocHost ( (void**) &theta0_h,  sizeof(float) * Nx       ); 
-  cudaMallocHost ( (void**) &kx_h,      sizeof(float) * Nx       ); 
-  cudaMallocHost ( (void**) &ky_h,      sizeof(float) * Nyc      );
-  cudaMallocHost ( (void**) &kz_h,      sizeof(float) * Nz       );
+  kz_outh   = (float*) malloc(sizeof(float) * Nz       );
+  kpar_outh = (float*) malloc(sizeof(float) * Nz       );
+  theta0_h  = (float*) malloc(sizeof(float) * Nx       ); 
+  kx_h      = (float*) malloc(sizeof(float) * Nx       ); 
+  ky_h      = (float*) malloc(sizeof(float) * Nyc      );
+  kz_h      = (float*) malloc(sizeof(float) * Nz       );
   cudaMalloc     ( (void**) &kx,        sizeof(float) * Nx       );
   cudaMalloc     ( (void**) &th0,       sizeof(float) * Nx       );
   cudaMalloc     ( (void**) &ky,        sizeof(float) * Nyc      );
   cudaMalloc     ( (void**) &kz,        sizeof(float) * Nz       );
-  cudaMallocHost ( (void**) &x_h,       sizeof(float) * Nx       );
-  cudaMallocHost ( (void**) &y_h,       sizeof(float) * Ny       );
+  x_h      = (float*) malloc(sizeof(float) * Nx       ); 
+  y_h      = (float*) malloc(sizeof(float) * Ny       );
   cudaMalloc     ( (void**) &kxs,       sizeof(float) * Nx * Nyc );
   checkCuda(cudaGetLastError());
 
@@ -131,14 +131,14 @@ Grids::~Grids() {
   if (kzp)             cudaFree(kzp);
   if (th0)             cudaFree(th0);
   
-  if (kpar_outh)       cudaFreeHost(kpar_outh);
-  if (kz_outh)         cudaFreeHost(kz_outh);
-  if (kx_outh)         cudaFreeHost(kx_outh);
-  if (kx_h)            cudaFreeHost(kx_h);
-  if (ky_h)            cudaFreeHost(ky_h);
-  if (kz_h)            cudaFreeHost(kz_h);
-  if (y_h)             cudaFreeHost(y_h);
-  if (theta0_h)        cudaFreeHost(theta0_h); 
+  if (kpar_outh)       free(kpar_outh);
+  if (kz_outh)         free(kz_outh);
+  if (kx_outh)         free(kx_outh);
+  if (kx_h)            free(kx_h);
+  if (ky_h)            free(ky_h);
+  if (kz_h)            free(kz_h);
+  if (y_h)             free(y_h);
+  if (theta0_h)        free(theta0_h); 
  
 }
 

src/laguerre_transform.cu
@@ -5,9 +5,9 @@ LaguerreTransform::LaguerreTransform(Grids* grids, int batch_size) :
   toGrid(nullptr), toSpectral(nullptr), roots(nullptr)
 {
   float * toGrid_h     = nullptr;  float * toSpectral_h = nullptr;  float * roots_h      = nullptr;
-  cudaMallocHost ((void**) &toGrid_h,     sizeof(float)*L*J);
-  cudaMallocHost ((void**) &toSpectral_h, sizeof(float)*L*J);
-  cudaMallocHost ((void**) &roots_h,      sizeof(float)*J);
+  toGrid_h     = (float*) malloc(sizeof(float)*L*J);
+  toSpectral_h = (float*) malloc(sizeof(float)*L*J);
+  roots_h      = (float*) malloc(sizeof(float)*J);
 
   cudaMalloc ((void**) &toGrid,     sizeof(float)*L*J);
   cudaMalloc ((void**) &toSpectral, sizeof(float)*L*J);
@@ -20,9 +20,9 @@ LaguerreTransform::LaguerreTransform(Grids* grids, int batch_size) :
   CP_TO_GPU (roots,      roots_h,      sizeof(float)*J);
 
   cublasCreate (&handle);
-  if (toGrid_h)     cudaFreeHost (toGrid_h);
-  if (toSpectral_h) cudaFreeHost (toSpectral_h);
-  if (roots_h)      cudaFreeHost (roots_h);
+  if (toGrid_h)     free (toGrid_h);
+  if (toSpectral_h) free (toSpectral_h);
+  if (roots_h)      free (roots_h);
 }
 
 LaguerreTransform::~LaguerreTransform()

commit e778b6d9a4c060c06f4081a405f6fb92dd9ca47b

minor bug fix. bug introduced in b205626

src/linear.cu
@@ -56,11 +56,11 @@ Linear::Linear(Parameters* pars, Grids* grids, Geometry* geo) :
     
     volDenom = 0.;  
     float *vol_fac_h;
-    vol_fac_h = (float*) malloc (sizeof(float) * nZ);
-    cudaMalloc (&vol_fac, sizeof(float) * nZ);
-    for (int i=0; i < nZ; i++) volDenom   += geo_->jacobian_h[i]; 
-    for (int i=0; i < nZ; i++) vol_fac_h[i]  = geo_->jacobian_h[i] / volDenom;
-    CP_TO_GPU(vol_fac, vol_fac_h, sizeof(float)*nZ);
+    vol_fac_h = (float*) malloc (sizeof(float) * grids_->Nz);
+    cudaMalloc (&vol_fac, sizeof(float) * grids_->Nz);
+    for (int i=0; i < grids_->Nz; i++) volDenom   += geo_->jacobian_h[i]; 
+    for (int i=0; i < grids_->Nz; i++) vol_fac_h[i]  = geo_->jacobian_h[i] / volDenom;
+    CP_TO_GPU(vol_fac, vol_fac_h, sizeof(float)*grids_->Nz);
     free(vol_fac_h);
   }
   