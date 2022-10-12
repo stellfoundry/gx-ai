07ffe78f48fc60f874081de4bbf725664d476885
Noah Mandell <nrmandell@gmail.com>
2022-10-12 17:46:53 -0400

    forgot to commit changes to parameters.h file for including krehm parameters in netcdf

--- include/parameters.h
+++ include/parameters.h
@@ -78,7 +78,7 @@ class Parameters {
   void set_jtwist_x0(float* shat);
 
   int nczid, nzid, ncresid, ncbid;
-  int nc_geo, nc_time, nc_ks, nc_vp, nc_rst, nc_dom, nc_diag;
+  int nc_geo, nc_time, nc_ks, nc_vp, nc_rst, nc_dom, nc_diag, nc_krehm;
   int nc_expert, nc_resize, nc_con, nc_frc, nc_bz, nc_ml, nc_sp, nc_spec;
   int p_HB, p_hyper_l, p_hyper_m, irho, nwrite, navg, nsave, igeo, nreal;
   int nz_in, nperiod, Zp, bishop, scan_number, iproc, icovering;
