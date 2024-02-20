209a4bb4fd11ba2473ec1e6e6cfacff0cd7f882f
Ian Abel <iga@physics.org>
2024-02-20 13:51:40 -0500

    tweak to bounds-checking on the momentsG API; needed for G->G(i) to work as expected, used from GradParallelPeriodic

--- include/moments.h
+++ include/moments.h
@@ -11,14 +11,21 @@ class MomentsG {
   ~MomentsG();
 
   // accessor function to get pointer to specific l,m,s of G array
-  // calling with no arguments gives pointer to beginning of G_lm
-  cuComplex* G(int l=0, int m=0) {
+  // calling with two arguments checks bounds for (l,m) in 2D
+  cuComplex* G(int l, int m) {
     assert(l<grids_->Nl && "Invalid moment requested: l out of bounds");
     assert(m<grids_->Nm+grids_->m_ghost && m>=-grids_->m_ghost && "Invalid moment requested: m out of bounds");
     return &G_lm[grids_->NxNycNz*(l + grids_->Nl*(m+grids_->m_ghost))]; // note shift by m_ghost! 
     // glm[ky, kx, z]
   }
   
+  // calling with one argument checks bounds in 1D i.e. (0,Nmoms)
+  cuComplex* G(int moment_idx = 0) {
+    assert( m >= 0 && m < grids_->Nmoms && "Invalid moment requested: moment index out of bounds" );
+    return &G_lm[grids_->NxNycNz*(moment_idx + grids_->Nl * grids_->m_ghost)]; // note shift by m_ghost! 
+    // glm[ky, kx, z]
+  }
+
   cuComplex * Gm(int m_loc) {   return G(0,m_loc);   }
 
   // accessor to G array including ghosts (never used) 
